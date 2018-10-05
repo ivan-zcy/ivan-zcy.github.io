243d9f3569824a0a238c4acd9b8c9b3dcfb6bef0
mac <mac@macdeMacBook-Pro.local>
2018-10-05 17:51:35 +0800

    Site updated: 2018-10-05 17:51:35

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,13 @@
 function _scroll(){
-    var acticles = $('content-area');
+    var acticles = $('main.content-area > div.article');
+    console.log(acticles);
     var height = window.innerHeight || document.body.offsetHeight;
     $(window).scroll(function () {
         acticles.each(function () {
             var top = this.getBoundingClientRect().top();
             console.log(top);
             if(top < height - 100){
-                $(this).addClass('animate');
+                $(this).addClass('animated');
             }
         })
     })
