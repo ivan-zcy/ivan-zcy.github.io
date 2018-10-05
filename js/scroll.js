677cd7bf45717b0130d50dc7200ca5de7583f0d9
mac <mac@macdeMacBook-Pro.local>
2018-10-05 17:42:47 +0800

    Site updated: 2018-10-05 17:42:46

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+function _scroll(){
+    var acticles = $('content-area');
+    var height = window.innerHeight || document.body.offsetHeight;
+    $(window).scroll(function () {
+        acticles.each(function () {
+            var top = this.getBoundingClientRect().top();
+            console.log(top);
+            if(top < height - 100){
+                $(this).addClass('animate');
+            }
+        })
+    })
+}
+_scroll();
