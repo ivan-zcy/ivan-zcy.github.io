7413d65ee87ea616a69b84c08041ce0162b15277
ivan <296763002@qq.com>
2019-01-31 14:54:27 +0800

    Site updated: 2019-01-31 14:54:26

--- js/search.js
+++ js/search.js
@@ -8,7 +8,7 @@ var searchFunc = function(path, search_id, content_id) {
             var datas = $( "entry", xmlResponse ).map(function() {
                 return {
                     title: $( "title", this ).text(),
-                    content: $("content",this).text(),
+                    // content: $("content",this).text(),
                     url: $( "url" , this).text()
                 };
             }).get();
@@ -25,27 +25,20 @@ var searchFunc = function(path, search_id, content_id) {
                 // 本地搜索主要部分
                 datas.forEach(function(data) {
                     var isMatch = true;
-                    var content_index = [];
+                    // var content_index = [];
                     var data_title = data.title.trim().toLowerCase();
-                    var data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
+                    // var data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
                     var data_url = data.url;
                     var index_title = -1;
-                    var index_content = -1;
+                    // var index_content = -1;
                     var first_occur = -1;
                     // 只匹配非空文章
-                    if(data_title != '' && data_content != '') {
+                    if(data_title != '') {
                         keywords.forEach(function(keyword, i) {
                             index_title = data_title.indexOf(keyword);
-                            index_content = data_content.indexOf(keyword);
-                            if( index_title < 0 && index_content < 0 ){
+                            //index_content = data_content.indexOf(keyword);
+                            if( index_title < 0){
                                 isMatch = false;
-                            } else {
-                                if (index_content < 0) {
-                                    index_content = 0;
-                                }
-                                if (i == 0) {
-                                    first_occur = index_content;
-                                }
                             }
                         });
                     }
@@ -53,28 +46,28 @@ var searchFunc = function(path, search_id, content_id) {
                     if (isMatch) {
                         //结果标签
                         str += "<li><a href='"+ data_url +"' class='search-result-title' target='_blank'>"+ "> " + data_title +"</a>";
-                        var content = data.content.trim().replace(/<[^>]+>/g,"");
-                        if (first_occur >= 0) {
-                            // 拿出含有搜索字的部分
-                            var start = first_occur - 6;
-                            var end = first_occur + 6;
-                            if(start < 0){
-                                start = 0;
-                            }
-                            if(start == 0){
-                                end = 10;
-                            }
-                            if(end > content.length){
-                                end = content.length;
-                            }
-                            var match_content = content.substr(start, end);
+                        //var content = data.content.trim().replace(/<[^>]+>/g,"");
+                        // if (first_occur >= 0) {
+                        //     // 拿出含有搜索字的部分
+                        //     var start = first_occur - 6;
+                        //     var end = first_occur + 6;
+                        //     if(start < 0){
+                        //         start = 0;
+                        //     }
+                        //     if(start == 0){
+                        //         end = 10;
+                        //     }
+                            // if(end > content.length){
+                            //     end = content.length;
+                            // }
+                            //var match_content = content.substr(start, end);
                             // 列出搜索关键字，定义class加高亮
-                            keywords.forEach(function(keyword){
-                                var regS = new RegExp(keyword, "gi");
-                                match_content = match_content.replace(regS, "<em class=\"search-keyword\">"+keyword+"</em>");
-                            })
-                            str += "<p class=\"search-result\">" + match_content +"...</p>"
-                        }
+                            // keywords.forEach(function(keyword){
+                            //     var regS = new RegExp(keyword, "gi");
+                            //     match_content = match_content.replace(regS, "<em class=\"search-keyword\">"+keyword+"</em>");
+                            // })
+                            // str += "<p class=\"search-result\">" + match_content +"...</p>"
+                        // }
                     }
                 })
                 $resultContent.innerHTML = str;
